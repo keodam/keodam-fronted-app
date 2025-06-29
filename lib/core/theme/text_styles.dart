07d345309a71f27752c202e96e4596897c78a662
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-06-29 12:07:56 +0900

    [Feat] Pretendard 기반 텍스트 스타일 정의 - AppTextStyle 클래스 생성 - 자주 사용하는 폰트 weight/size 조합 등록 '[regular/medium/bold][fontsize]' 예) regular12 - PretendardWeight 텍스트 스타일 구성

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'pretendard_weight.dart';
+
+class AppTextStyle {
+  static const regular10 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.regular,
+    fontSize: 10,
+  );
+
+  static const regular12 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.regular,
+    fontSize: 12,
+  );
+
+  static const medium12 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.medium,
+    fontSize: 12,
+  );
+
+  static const regular14 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.regular,
+    fontSize: 14,
+  );
+
+  static const bold14 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.bold,
+    fontSize: 14,
+  );
+
+  static const bold16 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.bold,
+    fontSize: 16,
+  );
+
+  static const medium16 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.medium,
+    fontSize: 16,
+  );
+
+  static const semiBold16 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.semiBold,
+    fontSize: 16,
+  );
+
+  static const bold18 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.bold,
+    fontSize: 18,
+  );
+
+  static const bold20 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.bold,
+    fontSize: 20,
+  );
+
+  static const extraBold20 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.extraBold,
+    fontSize: 20,
+  );
+
+  static const bold22 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.bold,
+    fontSize: 22,
+  );
+
+  static const bold24 = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: PretendardWeight.bold,
+    fontSize: 24,
+  );
+}
