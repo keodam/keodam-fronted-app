0aa22a61f686b55893000529cfd0a00dcbef0dd4
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-06-29 12:23:58 +0900

    [Feat] color.dart 파일 추가 - 앱 전역에서 사용할 색상 상수 정의 (figma에 등록된 이름 바탕)

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//button colors
+const pointColor529DFF = Color(0xff529DFF);
+const color02 = Color(0xff92C1FF);
+
+//stroke colors
+const strokeColor = Color(0xffF1F1F1);
+const backgroundColor01 = Color(0xffF1F1F1);
+
+//text colors
+const textBlack = Color(0xff020202);
+const textGray = Color(0xff707070);
+const textBlue02 = Color(0xff3A75FF);
+
+//etc
+const pureWhite = Color(0xffFFFFFF);
+const redColor = Color(0xffFF2B00);
