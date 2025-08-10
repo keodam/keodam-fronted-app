33c30b233fd4c0db62717d27561884aaffa44a0d
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-08-10 15:01:31 +0900

    fix: SupportDeveloperScreen layout 개선 - 백키 입력시 overflow 개선

--- lib/features/mypage/presentation/screens/support_developer_screen.dart
+++ lib/features/mypage/presentation/screens/support_developer_screen.dart
@@ -11,46 +11,50 @@ class SupportDeveloperScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: BasicAppBar(title: '개발자 후원하기'),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(26.0),
-              child: Image.asset('assets/images/splash_logo.png', width: 82),
-            ),
-            Text('계좌번호', style: AppTextStyle.bold16.copyWith(color: textBlack)),
-            const SizedBox(height: 5),
-            Text(
-              '신한은행 110-590-007883',
-              style: AppTextStyle.bold14.copyWith(color: textBlack),
-            ),
-            const SizedBox(height: 28),
-            Text(
-              textAlign: TextAlign.center,
-              '저희 서비스는 대학생들이 모여 대학 선후배, 그리고 \n 졸업생들과의 교류의 필요성을 인지하고 만든\n 대학 맞춤 커피챗서비스입니다. ',
-              style: AppTextStyle.regular14.copyWith(color: textGray),
-            ),
-            const SizedBox(height: 28),
-            Text(
-              textAlign: TextAlign.center,
-              '커담 서비스이 유용했다면, 작은 후원으로 큰 변화를 \n만들어주세요. 여러분의 후원은 더 많은 사람들에게 도움을 \n 줄 수 있는 동력이 됩니다.  ',
-              style: AppTextStyle.regular14.copyWith(color: textGray),
-            ),
-            const SizedBox(height: 40),
-            Text(
-              textAlign: TextAlign.center,
-              '지속적인 서비스 제공을 위해서 도움을 부탁드립니다.',
-              style: AppTextStyle.regular14.copyWith(color: textGray),
-            ),
-            const SizedBox(height: 40),
-            Expanded(
-              child: Container(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(26.0),
+                child: Image.asset('assets/images/splash_logo.png', width: 82),
+              ),
+              Text(
+                '계좌번호',
+                style: AppTextStyle.bold16.copyWith(color: textBlack),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                '신한은행 110-590-007883',
+                style: AppTextStyle.bold14.copyWith(color: textBlack),
+              ),
+              const SizedBox(height: 28),
+              Text(
+                textAlign: TextAlign.center,
+                '저희 서비스는 대학생들이 모여 대학 선후배, 그리고 \n 졸업생들과의 교류의 필요성을 인지하고 만든\n 대학 맞춤 커피챗서비스입니다. ',
+                style: AppTextStyle.regular14.copyWith(color: textGray),
+              ),
+              const SizedBox(height: 28),
+              Text(
+                textAlign: TextAlign.center,
+                '커담 서비스이 유용했다면, 작은 후원으로 큰 변화를 \n만들어주세요. 여러분의 후원은 더 많은 사람들에게 도움을 \n 줄 수 있는 동력이 됩니다.  ',
+                style: AppTextStyle.regular14.copyWith(color: textGray),
+              ),
+              const SizedBox(height: 40),
+              Text(
+                textAlign: TextAlign.center,
+                '지속적인 서비스 제공을 위해서 도움을 부탁드립니다.',
+                style: AppTextStyle.regular14.copyWith(color: textGray),
+              ),
+              const SizedBox(height: 40),
+              Container(
+                padding: const EdgeInsets.only(bottom: 40),
                 width: double.infinity,
                 decoration: BoxDecoration(color: color01),
                 child: Column(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.all(40.0),
+                      padding: const EdgeInsets.all(20.0),
                       child: Text(
                         '후원자베네핏',
                         style: AppTextStyle.bold16.copyWith(color: textBlack),
@@ -105,8 +109,8 @@ class SupportDeveloperScreen extends ConsumerWidget {
                   ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
